c660607f620a0d0a1e0f5233c8562a600ef68d75
KevinCorrea <rioezkzz@gmail.com>
2024-10-14 17:14:13 -0300

    Mensaje del commit

---
+++ animaciones.js
@@ -0,0 +1,30 @@
+const navbar = document.querySelector('.Navboxmenu');
+const linknav = document.querySelector('.navlinks');
+const activarlinknosotros = document.querySelector('.linknosotros')
+const navmenuaction = document.querySelector('.navlinksbox');
+const navmenuicon = document.querySelector('.navmenu');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 300){
+        navbar.classList.add('navmenuon');
+        linknav.classList.add('navlinksonscroll');
+       
+    }else{
+        navbar.classList.remove('navmenuon');
+        linknav.classList.remove('navlinksonscroll');
+        
+    }
+})
+
+
+
+
+navmenuicon.addEventListener('click', (e) => {
+    if(navmenuicon.click){
+        navmenuaction.classList.toggle('mobilenavmenuocultar');
+    }
+    else if(!navmenuicon.contains(e.target)) {
+        navmenuaction.classList.remove('mobilenavmenuocultar');
+    }
+})
+
